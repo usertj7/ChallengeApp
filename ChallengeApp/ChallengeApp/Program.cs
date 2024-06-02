d8dc14ce50d7ba6398d28536bd6bdfe7da55aaf4
Tymek <jacaktymoteusz@gmail.com>
2024-06-02 19:31:56 +0200

    zadanie domowe dzien 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,20 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿var name = "Jan";
+var sex = "male";
+var age = 17;
+
+if (sex == "female" && age < 30)
+{
+    Console.WriteLine("Kobieta poniżej 30 lat");
+}
+else if (name == "Ewa" && age == 30)
+{
+    Console.WriteLine("Ewa, lat 30");
+}
+else if (sex == "male" && age < 18)
+{
+    Console.WriteLine("Niepełnoletni mężczyzna");
+}
+else
+{
+    
+}
