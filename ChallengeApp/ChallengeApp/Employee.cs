95e3a8340857f63d528daf556465a68dfc6876a4
Tymek <jacaktymoteusz@gmail.com>
2024-06-10 16:22:18 +0200

    Homework - day 6

---
+++ ChallengeApp/ChallengeApp/Employee.cs
@@ -0,0 +1,31 @@
+﻿namespace ChallengeApp
+{
+    internal class Employee
+    {
+        public string Name { get; private set; }
+        public string LastName { get; private set; }
+        public int Age { get; private set; }
+        private List<int> Grades = new List<int>();
+
+        public void AddGrade(int Grade)
+        {
+            Grades.Add(Grade);
+        }
+        public int SumGrades { get { return Grades.Sum(); } }
+
+        public void Display()
+        {
+            Console.WriteLine("Name:"+Name);
+            Console.WriteLine("Last Name: "+LastName);
+            Console.WriteLine("Age: "+Age);
+            Console.WriteLine("Score: " + Grades.Sum());
+        }
+        public Employee(string name, string lastname, int age)
+        {
+            this.Name = name;
+            this.LastName = lastname;
+            this.Age = age;
+
+        }
+    }
+}
--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,35 @@
-﻿
-int number = 4566;
-int[] numbers = new int[10];
-string numberinstring = number.ToString();
-Char[] letter = numberinstring.ToArray();
+﻿using ChallengeApp;
 
-foreach (var let in letter)
+List<Employee> employes = new List<Employee>();
+employes.Add(new Employee("marek", "nowak", 23));
+employes.Add(new Employee("andrzej", "kowalski", 43));
+employes.Add(new Employee("tomasz", "kot", 32));
+var rand = new Random();
+List<int> results = new List<int>();
+results.Add(0);
+int MaxGrade = 0;
+
+for (int i = 0; i < 5; i++)
 {
-    numbers[let - '0'] += 1;
+    foreach (var emp in employes)
+        emp.AddGrade(rand.Next(1, 10));
 }
 
-for (int i = 0; i < 10; i++)
+for (int i = 0; i < employes.Count(); i++)
 {
-    Console.WriteLine(i.ToString() + " => " + numbers[i]);
+    if (employes[i].SumGrades > MaxGrade)
+    {
+        MaxGrade = employes[i].SumGrades;
+        results.Clear();
+        results.Add(i);
+    }
+    else if (employes[i].SumGrades == MaxGrade)
+    {
+        results.Add(i);
+    }
 }
 
+foreach (var res in results)
+{
+    employes[res].Display();
+}
