40dd326a719f6da4e1ef807932b67640f9638a38
Tymek <jacaktymoteusz@gmail.com>
2024-06-19 20:38:29 +0200

    homework - day 8

---
+++ ChallengeApp/Employee.Tests/TypeTests.cs
@@ -0,0 +1,42 @@
+﻿
+using ChallengeApp;
+
+namespace Employe.Tests
+{
+    public class TypeTests
+    {
+        [Test]
+        public void TwoIntigersCompare()
+        {
+            //arrange
+            int number1 = 2;
+            int number2 = 2;
+            //assert
+            Assert.AreEqual(number2, number1);
+        }
+        [Test]
+        public void TwoFloatsCompare()
+        {
+            //arrange
+            float number1 = 3.54F;
+            float number2 = 3.54F;
+            //assert
+            Assert.AreEqual(number2, number1);
+        }
+        [Test]
+        public void TwoStringsCompare()
+        {
+            //arrange
+            var employee1 = GetEmployee("marek", "ddd");
+            var employee2 = GetEmployee("marek", "ddd"); 
+            //assert
+            Assert.AreEqual(employee1.Name, employee2.Name);
+            Assert.AreEqual(employee1.LastName, employee2.LastName);
+        }
+      private Employee GetEmployee(string name, string lastname)
+        {
+            return new Employee(name, lastname, 44);
+        }
+
+    }
+}
