efc3312d14839e94cae23dae7f93d53fbcdf1264
Tymek <jacaktymoteusz@gmail.com>
2024-06-23 18:17:11 +0200

    homework - day 11

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -2,13 +2,28 @@
 
 
 var employee = new Employee("marek", "kot");
-employee.AddGrade(200);
+employee.AddGrade(5);
 employee.AddGrade("8");
 employee.AddGrade(3.141592653589793238463);
 var statistics = employee.GetStatistics();
 Console.WriteLine($"average: {statistics.Average:N3}");
 Console.WriteLine($"Min: {statistics.Min}");
 Console.WriteLine($"Max: {statistics.Max}");
+
+var statistics1 = employee.GetStatisticsWithFor();
+Console.WriteLine($"average: {statistics1.Average:N3}");
+Console.WriteLine($"Min: {statistics1.Min}");
+Console.WriteLine($"Max: {statistics1.Max}");
+
+var statistics2 = employee.GetStatisticsWithDoWhile();
+Console.WriteLine($"average: {statistics2.Average:N3}");
+Console.WriteLine($"Min: {statistics2.Min}");
+Console.WriteLine($"Max: {statistics2.Max}");
+
+var statistics3 = employee.GetStatisticsWithWhile();
+Console.WriteLine($"average: {statistics3.Average:N3}");
+Console.WriteLine($"Min: {statistics3.Min}");
+Console.WriteLine($"Max: {statistics3.Max}");
 
 
 
